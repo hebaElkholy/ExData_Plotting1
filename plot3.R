b14461173ffe13f8745764a4d27ab61e69445166
Heba <heba.elkholy@algoproviders.com>
2014-08-11 14:31:49 +0200

    corrections

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-data <- read.table("../household_power_consumption.txt",header= TRUE,sep=";")
+data <- read.table("../household_power_consumption.txt",header= TRUE,sep=";",na.strings="?")
 
 data2 <- data
 
@@ -6,20 +6,21 @@ data2$Date <- as.Date(data2$Date,format= "%d/%m/%Y")
 data2$Time <- strptime(data2$time)
 
 
-myData<-data2[data2$Date %in% as.Date(c('2007-02-01', '2007-02-02')),]
-
-myData[myData$Global_active_power=="?"]<-NA
-
-globalPower<-myData[["Global_active_power"]]
-globalPower[globalPower=="?"]<-NA
 
 
+myData <- subset(data2, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-plot(myData$Date,as.numeric(paste(myData$Sub_metering_1)),col="black", type="l",xlab="",ylab="Energy sub metering")
-lines(myData$Date,as.numeric(paste(myData$Sub_metering_2)),col="red")
-lines(myData$Date,as.numeric(paste(myData$Sub_metering_2)),col="blue")
+datetime <- paste(myData$Date, myData$Time)
+myData$Datetime <- as.POSIXct(datetime)
+
+
+
+
+plot(myData$Datetime,myData$Sub_metering_1,col="black", type="l",xlab="",ylab="Energy sub metering")
+lines(myData$Datetime,myData$Sub_metering_2,col="red")
+lines(myData$Datetime,myData$Sub_metering_3,col="blue")
 legend("topright", lty=c(1,1,1),col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
 
-dev.copy(png, file = "plot3.png")
+dev.copy(png, file = "plot3.png",height=480, width=480)
 dev.off()
