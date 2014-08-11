6d80bf0e76975acf1c70783c7ead09ebe5e94f43
Heba <heba.elkholy@algoproviders.com>
2014-08-11 14:21:34 +0200

    corrections

--- plot2.R
+++ plot2.R
@@ -1,6 +1,4 @@
-
-
-data <- read.table("../household_power_consumption.txt",header= TRUE,sep=";")
+data <- read.table("../household_power_consumption.txt",header= TRUE,sep=";",na.strings="?")
 
 data2 <- data
 
@@ -8,18 +6,11 @@ data2$Date <- as.Date(data2$Date,format= "%d/%m/%Y")
 data2$Time <- strptime(data2$time)
 
 
-myData<-data2[data2$Date %in% as.Date(c('2007-02-01', '2007-02-02')),]
-
-myData[myData$Global_active_power=="?"]<-NA
-
-globalPower<-myData[["Global_active_power"]]
-globalPower[globalPower=="?"]<-NA??
+myData <- subset(data2, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 
+plot(myData$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 
-
-plot(myData$Date,as.numeric(paste(myData$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)")
-
-dev.copy(png, file = "plot2.png")
+dev.copy(png, file = "plot2.png",height=480, width=480)
 dev.off()
 
